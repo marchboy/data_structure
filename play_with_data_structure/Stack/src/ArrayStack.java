d5f52215b253405d8fb70cd6e7f118e22b3c11c6
jinjun.gui <jjgdut@163.com>
2019-07-03 19:24:07 +0800

    updated

--- play_with_data_structure/Stack/src/ArrayStack.java
+++ play_with_data_structure/Stack/src/ArrayStack.java
@@ -1,6 +1,8 @@
 package play_with_data_structure.Stack.src;
 
-public class ArrayStack<E> implements Stack<E>{
+public class ArrayStack<E>  implements Stack<E>{
+
+
     ArrayGeneric<E> array;
     
     public ArrayStack(int capacity){
@@ -11,12 +13,12 @@ public class ArrayStack<E> implements Stack<E>{
         array = new ArrayGeneric<>();
     }
 
-    @Override
+    // @Override
     public int getSize(){
         return array.getSize();
     }
 
-    @Override
+    // @Override
     public boolean isEmpty(){
         return array.isEmpty();
     }
@@ -25,22 +27,22 @@ public class ArrayStack<E> implements Stack<E>{
         return array.getCapacity();
     }
 
-    @Override
+    // @Override
     public void push(E e){
         array.addLast(e);
     }
 
-    @Override
+    // @Override
     public E pop(){
         return array.removeLast();
     }
 
-    @Override
+    // @Override
     public E peek(){
         return array.getLast();
     }
 
-    @Override
+    // @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
         res.append("Stack: ");
@@ -50,7 +52,7 @@ public class ArrayStack<E> implements Stack<E>{
             if(i != array.getSize() - 1)
                 res.append(", ");
         }
-        res.append("] top");
+        res.append("] top\n");
 
         return res.toString();
     }
--- play_with_data_structure/Stack/src/Stack.java
+++ play_with_data_structure/Stack/src/Stack.java
@@ -1,6 +1,7 @@
 package play_with_data_structure.Stack.src;
 
 public interface Stack<E>{
+
     int getSize();
     boolean isEmpty();
     void push(E e);
